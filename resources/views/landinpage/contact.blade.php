36e5bde5c00bfc14160215aecdd25a3b5e019824
Daniel Vazquez <dcva9565@hotmail.com>
2022-08-31 19:39:54 -0500

    git commit -m ¨¨ git commit -m

--- resources/views/landinpage/contact.blade.php
+++ resources/views/landinpage/contact.blade.php
@@ -5,7 +5,6 @@
         </div>
     <div class="bg-white m-5 col-st w-full md:w-1/2 md:max-w-full mx-auto">
         <div class="p-6 border border-gray-300 sm:rounded-md">
-            <form method="POST">
                 <label class="block mb-6">
                     <span class="text-gray-700">Tu nombre</span>
                     <input type="text" name="name" class="
@@ -52,7 +51,7 @@
           " rows="3" placeholder="Hola buen dia..."></textarea>
                 </label>
                 <div class="mb-6">
-                    <button type="submit" class="
+                    <button class="
             h-10
             px-5
             text-indigo-100
